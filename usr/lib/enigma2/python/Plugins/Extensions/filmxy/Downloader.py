8a213d7fecd26d379f8bb9f4f6f2668f682f9876
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2022-11-20 13:20:06 +0100

    Add files via upload

--- usr/lib/enigma2/python/Plugins/Extensions/filmxy/Downloader.py
+++ usr/lib/enigma2/python/Plugins/Extensions/filmxy/Downloader.py
@@ -1,109 +1,91 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+from os import unlink
+import requests
+from twisted.internet import reactor
+from urllib.request import urlopen, Request
 
-from twisted.web import client
-from twisted.internet import reactor, defer, ssl
-
-
-# from twisted.internet import reactor, defer
-# from twisted.web import client
-from twisted.web.client import HTTPClientFactory, downloadPage
-
-class myHTTPClientFactory(HTTPClientFactory):
-	def __init__(self, url, method='GET', postdata=None, headers=None,
-	agent="SHOUTcast", timeout=0, cookies=None,
-	followRedirect=1, lastModified=None, etag=None):
-		HTTPClientFactory.__init__(self, url, method=method, postdata=postdata,
-		headers=headers, agent=agent, timeout=timeout, cookies=cookies, followRedirect=followRedirect)
+from enigma import eTimer
 
 
-def sendUrlCommand(url, contextFactory=None, timeout=60, *args, **kwargs):
-	if hasattr(client, '_parse'):
-			scheme, host, port, path = client._parse(url)
-	else:
-		# _URI class renamed to URI in 15.0.0
+class DownloadWithProgress:
+	def __init__(self, url, outputFile):
+		self.url = url
+		self.outputFile = outputFile
+		self.userAgent = "Enigma2 HbbTV/1.1.1 (+PVR+RTSP+DL;OpenATV;;;)"
+		# self.agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5"
+		self.totalSize = 0
+		self.progress = 0
+		self.progressCallback = None
+		self.endCallback = None
+		self.errorCallback = None
+		self.stopFlag = False
+		self.timer = eTimer()
+		self.timer.callback.append(self.reportProgress)
+
+	def start(self):
 		try:
-			from twisted.web.client import _URI as URI
-		except ImportError:
-			from twisted.web.client import URI
-		uri = URI.fromBytes(url)
-		scheme = uri.scheme
-		host = uri.host
-		port = uri.port
-		path = uri.path
-	factory = myHTTPClientFactory(url, *args, **kwargs)
-	reactor.connectTCP(host, port, factory, timeout=timeout)
-	return factory.deferred
+			request = Request(self.url, None, {"User-agent": self.userAgent})
+			feedFile = urlopen(request)
+			metaData = feedFile.headers
+			self.totalSize = int(metaData.get("Content-Length", 0))
+			# Set the transfer block size to a minimum of 1K and a maximum of 1% of the file size (or 128KB if the size is unknown) else use 64K.
+			self.blockSize = max(min(self.totalSize // 100, 1024), 131071) if self.totalSize else 65536
+		except OSError as err:
+			if self.errorCallback:
+				self.errorCallback(err)
+			return self
+		reactor.callInThread(self.run)
+		return self
+
+	def run(self):
+		# requests.Response object = requests.get(url, params=None, allow_redirects=True, auth=None, cert=None, cookies=None, headers=None, proxies=None, stream=False, timeout=None, verify=True)
+		response = requests.get(self.url, headers={"User-agent": self.userAgent}, stream=True)  # Streaming, so we can iterate over the response.
+		try:
+			with open(self.outputFile, "wb") as fd:
+				for buffer in response.iter_content(self.blockSize):
+					if self.stopFlag:
+						response.close()
+						fd.close()
+						unlink(self.outputFile)
+						return True
+					self.progress += len(buffer)
+					if self.progressCallback:
+						self.timer.start(0, True)
+					fd.write(buffer)
+			if self.endCallback:
+				self.endCallback(self.outputFile)
+		except OSError as err:
+			if self.errorCallback:
+				self.errorCallback(err)
+		return False
+
+	def stop(self):
+		self.stopFlag = True
+
+	def reportProgress(self):
+		self.progressCallback(self.progress, self.totalSize)
+
+	def addProgress(self, progressCallback):
+		self.progressCallback = progressCallback
+
+	def addEnd(self, endCallback):
+		self.endCallback = endCallback
+
+	def addError(self, errorCallback):
+		self.errorCallback = errorCallback
+
+	def setAgent(self, userAgent):
+		self.userAgent = userAgent
+
+	def addErrback(self, errorCallback):  # Temporary supprt for deprecated callbacks.
+		print("[Downloader] Warning: DownloadWithProgress 'addErrback' is deprecated use 'addError' instead!")
+		self.errorCallback = errorCallback
+		return self
+
+	def addCallback(self, endCallback):  # Temporary supprt for deprecated callbacks.
+		print("[Downloader] Warning: DownloadWithProgress 'addCallback' is deprecated use 'addEnd' instead!")
+		self.endCallback = endCallback
+		return self
 
 
-class HTTPProgressDownloader(client.HTTPDownloader):
-    def __init__(self, url, outfile, headers=None):
-        client.HTTPDownloader.__init__(self, url, outfile, headers=headers, agent="Enigma2 HbbTV/1.1.1 (+PVR+RTSP+DL;OpenATV;;;)")
-        self.status = None
-        self.progress_callback = None
-        self.deferred = defer.Deferred()
-
-    def noPage(self, reason):
-        if self.status == "304":
-            print(reason.getErrorMessage())
-            client.HTTPDownloader.page(self, "")
-        else:
-            client.HTTPDownloader.noPage(self, reason)
-
-    def gotHeaders(self, headers):
-        if self.status == "200":
-            # if headers.has_key("content-length"):
-            if "content-length" in headers:
-                self.totalbytes = int(headers["content-length"][0])
-            else:
-                self.totalbytes = 0
-            self.currentbytes = 0.0
-        return client.HTTPDownloader.gotHeaders(self, headers)
-
-    def pagePart(self, packet):
-        if self.status == "200":
-            self.currentbytes += len(packet)
-        if self.totalbytes and self.progress_callback:
-            self.progress_callback(self.currentbytes, self.totalbytes)
-        return client.HTTPDownloader.pagePart(self, packet)
-
-    def pageEnd(self):
-        return client.HTTPDownloader.pageEnd(self)
-
-
-class downloadWithProgress:
-    def __init__(self, url, outputfile, contextFactory=None, *args, **kwargs):
-        if hasattr(client, '_parse'):
-            scheme, host, port, path = client._parse(url)
-        else:
-            url = byte(url)
-            # from twisted.web.client import _URI
-            try:
-                from twisted.web.client import URI            
-            except ImportError as e:
-                from twisted.web.client import _URI as URI
-                print(str(e))
-            uri = URI.fromBytes(url)
-            # uri = _URI.fromBytes(url)
-            scheme = uri.scheme
-            host = uri.host
-            port = uri.port
-            path = uri.path
-
-        self.factory = HTTPProgressDownloader(url, outputfile, *args, **kwargs)
-        if scheme == "https":
-            self.connection = reactor.connectSSL(host, port, self.factory, ssl.ClientContextFactory())
-        else:
-            self.connection = reactor.connectTCP(host, port, self.factory)
-
-    def start(self):
-        return self.factory.deferred
-
-    def stop(self):
-        if self.connection:
-            print("[stop]")
-            self.connection.disconnect()
-
-    def addProgress(self, progress_callback):
-        print("[addProgress]")
-        self.factory.progress_callback = progress_callback
+class downloadWithProgress(DownloadWithProgress):  # Class names should start with a Capital letter, this catches old code until that code can be updated.
+	pass
