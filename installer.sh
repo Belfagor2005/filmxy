04b0b74978a4d6c07ad779efb0c8fc917cb1ac02
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-07-27 16:33:15 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -86,7 +86,7 @@ if [ $OSTYPE != "DreamOs" ]; then
 fi
 sleep 2
 
-wget --no-check-certificate --no-cache --no-dns-cache 'https://github.com/Belfagor2005/filmxy/archive/refs/heads/main.tar.gz'
+wget --no-check-certificate 'https://github.com/Belfagor2005/filmxy/archive/refs/heads/main.tar.gz'
 tar -xzf main.tar.gz
 cp -r 'filmxy-main/usr' '/'
 set +e
